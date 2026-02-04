cdfa9911bff39d9cd4faf4ace837111b59cbc0e9
yl4070 <yl4070@rit.edu>
2026-02-04 01:20:47 -0500

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,13 @@ using AlgebraOfInference
 using Test
 
 @testset "AlgebraOfInference.jl" begin
-    sp1 = Spec(:a)
-    sp2 = Spec("b")
+    sp1 = specify(:a)
+    sp2 = specify("b")
     sl = (sp1 + sp2)
     @test sl.specs[1] == sp1
     @test sl.specs[2] == sp2
 
-    sp3 = Spec("c")
+    sp3 = specify("c")
     sl2 = sl + sp3 
     @test sl2.specs[3] == sp3
 
